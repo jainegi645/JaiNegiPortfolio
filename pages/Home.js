b0d9692246e2620922b14ea5a12e8b142d924052
jainegi <jainegi645@gmail.com>
2022-04-30 21:20:09 +0530

    solved error

--- pages/Home.js
+++ pages/Home.js
@@ -4,14 +4,12 @@ import CatalougeHome from "../components/Home/CatalougeHome";
 import LiteratureHome from "../components/Home/LiteratureHome";
 import ExhibitionHome from "../components/Home/ExhibitionHome";
 import Alert from "../components/Alert/Alert";
-import MobileNav from "../components/Navbar/MobileNav";
 
 const Home = () => {
   return (
     <div className="bg-slate-50">
       {/* <Alert/> */}
       <Carousel />
-      {/* <MobileNav/> */}
     
       
     </div>
